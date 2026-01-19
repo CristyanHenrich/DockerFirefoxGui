00589a1a51573980156a1965c50d26c6058e5f9a
Cristyan Henrich <cristyanhenrich16@gmail.com>
2026-01-19 13:29:47 -0300

    update

--- FirefoxGUI/entrypoint.sh
+++ FirefoxGUI/entrypoint.sh
@@ -4,16 +4,19 @@
 rm -f /tmp/.X99-lock
 rm -rf /tmp/.X11-unix/X99
 
-# Start Xvfb
-Xvfb :99 -ac -screen 0 1280x1024x24 &
+# Start DBUS (necessary for XFCE and modern apps)
+export $(dbus-launch)
 
-# Wait for Xvfb
+# Start Xvfb (Virtual Framebuffer)
+Xvfb :99 -ac -screen 0 1920x1080x24 &
+
+# Wait for Xvfb to be ready
 sleep 2
 
-# Start x11vnc
+# Start x11vnc (VNC Server)
 x11vnc -display :99 -forever -nopw -rfbport 5900 &
 
 export DISPLAY=:99
 
-# Start the command (Firefox)
+# Execute the CMD (startxfce4)
 exec "$@"
